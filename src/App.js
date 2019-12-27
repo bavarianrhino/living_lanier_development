d2ffbae58851f1d2fcd2931bbcd1b595a9748c75
bavarianrhino <rjriesenberger@gmail.com>
2019-12-26 23:46:48 -0500

    added css and background image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import livinglaniersunset from './assets/livinglaniersunset.jpg';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                {/* <img src={logo} className="App-logo" alt="logo" /> */}
-                <h1>Living Lanier Realty</h1>
-                <p>Coming Soon!</p>
-            </header>
+    return (        	
+        <div className="main-area center-text" style={{ opacity: '.8', backgroundImage: `url(${livinglaniersunset})` }}>
+            <div className="display-table">
+                <div className="display-table-cell">
+
+                    <h1 className="title font-white"><b>Comming Soon</b></h1>
+                    <p className="desc font-white">Living Lanier Realty</p>
+
+                    {/* <!-- social-btn --> */}
+                    <ul className="social-btn font-white">
+                        <li><a href="#">facebook</a></li>
+                        <li><a href="#">twitter</a></li>
+                        <li><a href="#">google</a></li>
+                        <li><a href="#">instagram</a></li>
+                    </ul>
+
+                {/* </div> <!-- display-table --> */}
+                </div>
+            {/* </div> <!-- display-table-cell --> */}
+            </div>
+        {/* </div> <!-- main-area --> */}
         </div>
     );
 }
